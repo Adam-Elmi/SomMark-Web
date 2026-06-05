3f83e85f4792481aafda87289d08242abfd00d58
Adam-Elmi <Adamcade123@gmail.com>
2026-06-05 17:37:20 +0300

    chore: update create-sommark-web starter template

--- create-sommark-web/template/smark.config.js
+++ create-sommark-web/template/smark.config.js
@@ -1,6 +1,1 @@
-export default {
-    placeholders: {
-        //  Tells the sommark lsp(language server) where to find the page to import
-        page: "./src/pages/index.smark"
-    }
-}
+export default {}
